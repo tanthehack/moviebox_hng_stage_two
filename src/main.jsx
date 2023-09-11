6e7caa428d59b7f0249e89db011f0e53b2763f0d
tanthehack <31630356+tanthehack@users.noreply.github.com>
2023-09-11 02:54:18 +0100

    consfigured router with routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,22 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
 import { Provider } from 'react-redux'
 import { store } from './api/store.js'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
+import { TopMovies } from './routes/landing/topMovies.js'
+import { SearchResults } from './routes/landing/searchResults.js'
+import { Movie } from './routes/details/movie.js'
+import { MoviesLayout } from './components/layouts/movies.jsx'
+
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App />
-  },
-  {
-    path: "movies",
     element: <App />,
     children: [
       {
+        index: true,
+        path: "/",
+        element: <TopMovies />
+      },
+      {
+        path: ":query",
+        element: <SearchResults />
+      }
+    ]
+  },
+  {
+    path: "movies",
+    element: <MoviesLayout />,
+    children: [
+      {
         path: ":movieId",
-        element: <App />
+        element: <Movie />
       }
     ]
   }
