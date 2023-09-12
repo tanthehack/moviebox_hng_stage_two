e7f66fa3b87ac47640a7133d401e95bd7cc62ac7
tanthehack <31630356+tanthehack@users.noreply.github.com>
2023-09-12 10:04:54 +0100

    fix: unwanted horizontal scrollbar

--- src/components/layouts/landing.jsx
+++ src/components/layouts/landing.jsx
@@ -3,7 +3,7 @@ import { Nav } from "../global/nav"
 
 export const LandingLayout = ({ children }) => {
     return (
-        <main className="flex flex-col gap-[100px] font-dm w-[100dvw]">
+        <main className="flex flex-col gap-[100px] font-dm">
             <Nav />
             {children}
             <Footer />
