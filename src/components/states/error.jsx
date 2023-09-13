b9567c0ba820ef6a30ee7029539136f3643038c4
tanthehack <31630356+tanthehack@users.noreply.github.com>
2023-09-13 22:37:56 +0100

    add: error page

--- src/components/states/error.jsx
+++ src/components/states/error.jsx
@@ -1,9 +1,28 @@
-import error from '../../assets/error.svg'
-import { useRouteError } from "react-router-dom";
+import errorImg from '../../assets/error.svg'
+import { useNavigate, useRouteError } from "react-router-dom";
+import * as Icon from '@heroicons/react/24/solid'
+import { Button } from '../global/button';
 
 export const ErrorState = () => {
-    <section className="w-[100vw] h-[100vh] bg-gray-900">
-        hi
-        <img src={error} />
-    </section>
+    const error = useRouteError();
+    const navigate = useNavigate()
+    return (
+        <section className='lg:h-[100dvh]'>
+            <div className="p-[30px] flex items-center gap-2">
+                <Button
+                    icon={<Icon.ChevronLeftIcon />}
+                    iconOnly
+                    widthFit
+                    onClick={() => navigate(-1, { replace: true })}
+                />
+                <h1 className="lg:text-4xl text-xl font-bold">Search Results</h1>
+            </div>
+            <div className="flex flex-col items-center justify-center text-xl">
+                <img src={errorImg} className='w-[200px] lg:w-[300px]' />
+                <span className="text-3xl font-bold">Oops!</span>
+                <p className=''>Seems like we've encountered a problem</p>
+                <p className='font-bold'>{error?.status}: {error?.message || error?.statusText}</p>
+            </div>
+        </section>
+    )
 }
